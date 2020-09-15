e87c26f52166cd618864dedadc58b59d6fd5ae07
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-09-14 20:45:07 -0400

    Se creó la clase principal, el DataContext y se anexó a la base de datos.

---
+++ PracticaP1/PracticaP1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PracticaP1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PracticaP1/PracticaP1/Models/Eid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticaP1.Models
+
+{
+
+    public enum CategoryType
+    {
+        Cine=10,
+        Plaza=20,
+        Parque=30,
+        Teatro=40,
+        Colegio=50
+    }
+
+    public class Eid
+    {
+
+        [Key]
+        public int eidID { get; set; }
+
+
+        [Required]
+        [DisplayName("Nombre Completo")]
+        [StringLength(24,MinimumLength =2)]
+        public string Friendofeid { get; set; }
+
+        [Required]
+        public CategoryType Place { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [DisplayName("Cumpleaños")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{00:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthday { get; set; }
+    }
+}
